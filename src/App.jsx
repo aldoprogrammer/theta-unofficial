2fbe25b6b2bf602e29411bb048053a8677e15ece
aldoprogrammer <aldobesma@gmail.com>
2024-07-21 15:50:45 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import { useMetamask, useAddress } from '@thirdweb-dev/react';
-import SignInSignUpModal from './modal/SignInSignUpModal';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -8,22 +5,7 @@ import Swap from './pages/swap';
 import User from './pages/User';
 import UploadWithoutURL from './components/FormWithOutURL';
 
-
 function App() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const address = useAddress();
-    const connectWithMetamask = useMetamask();
-
-    const handleConnect = async () => {
-        try {
-            await connectWithMetamask();
-            if (address) {
-                setIsModalOpen(true);
-            }
-        } catch (error) {
-            console.error('MetaMask connection failed:', error);
-        }
-    };
 
     return (
         <>
@@ -35,10 +17,6 @@ function App() {
                     <Route path="/User" element={<User />} />
                     <Route path="/Form" element={<UploadWithoutURL />} />
                 </Routes>
-                <button onClick={handleConnect}>
-                {address ? 'Connected' : 'Connect with MetaMask'}
-            </button>
-            <SignInSignUpModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
             </Router>
             
             </>
